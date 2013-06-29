c84b1d8c113957e4dd89f6aec95569182bac67ea
Phil Seeley <phil@oc0428136010.ibm.com>
2013-06-29 12:14:56 +1000

    Added menu option to scan an image.

--- src/name/seeley/phil/hgfa/MainActivity.java
+++ src/name/seeley/phil/hgfa/MainActivity.java
@@ -1,6 +1,7 @@
 package name.seeley.phil.hgfa;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
@@ -16,17 +17,29 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 @SuppressLint("SimpleDateFormat")
 public class MainActivity extends Activity
@@ -35,8 +48,10 @@ public class MainActivity extends Activity
 
   private static final String PUBLIC_PEM = "MFUwEwYHKoZIzj0CAQYIKoZIzj0DAQQDPgAEZFvqdcZ+KiZIxH7/vOruEkK5IP3WwZtoiLL+chQjEzb5nSIjLKKATk2Utz/SpQmS0EvOGTKm/EPCmb6j";
 
-  private static String T = "HGFA";
-      
+  private static final String T = "HGFA";
+  private static final int SCAN_IMAGE_REQ = 10;
+  private static final int SELECT_IMAGE_REQ = 20;
+
   private static PublicKey publicKey;
   private static SimpleDateFormat dateFormatter;
   private static Date now = new Date();
@@ -47,7 +62,7 @@ public class MainActivity extends Activity
         new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
 
     dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
-    
+
     try
     {
       KeyFactory fact = KeyFactory.getInstance("ECDSA");
@@ -55,7 +70,8 @@ public class MainActivity extends Activity
           PUBLIC_PEM, Base64.DEFAULT));
       publicKey = fact.generatePublic(publicKeySpec);
 
-    } catch (GeneralSecurityException e)
+    }
+    catch (GeneralSecurityException e)
     {
       Log.e(T, e.toString());
     }
@@ -76,114 +92,188 @@ public class MainActivity extends Activity
     return true;
   }
 
-  public void scan(View view)
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+    // Handle item selection
+    switch (item.getItemId())
+    {
+    case R.id.menu_scan_image:
+      startImageSelect();
+      return true;
+    default:
+      return super.onOptionsItemSelected(item);
+    }
+  }
+
+  public void startScan(View view)
   {
     Intent intent = new Intent("com.google.zxing.client.android.SCAN");
     intent.putExtra("com.google.zxing.client.android.SCAN.SCAN_MODE",
         "QR_CODE_MODE");
-    startActivityForResult(intent, 0);
+    startActivityForResult(intent, SCAN_IMAGE_REQ);
+  }
+
+  public void startImageSelect()
+  {
+    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+    photoPickerIntent.setType("image/*");
+    startActivityForResult(photoPickerIntent, SELECT_IMAGE_REQ);
   }
 
   public void onActivityResult(int requestCode, int resultCode, Intent intent)
   {
-    if (requestCode == 0)
+    if (resultCode == RESULT_OK)
     {
-      if (resultCode == RESULT_OK)
+      TextView infoTextView = (TextView) findViewById(R.id.text_info);
+      infoTextView.setText(null);
+
+      String contents = null;
+
+      switch (requestCode)
       {
-        String contents = intent.getStringExtra("SCAN_RESULT");
-
-        BufferedReader reader = new BufferedReader(new StringReader(contents));
-
-        StringBuffer data = new StringBuffer();
-        
-        String from[] = {"tag", "icon", "value"};
-        int to[] = {R.id.tag, R.id.icon, R.id.value};
-        
-        List<Map<String, Object>> items = new ArrayList<Map<String, Object>>();
-        
-        SimpleAdapter adapter = new SimpleAdapter(this, items, R.layout.list_view, from, to);
-
-        ListView infoListView = (ListView) findViewById(R.id.listView);
-        ImageView imageView = (ImageView) findViewById(R.id.image_result);
-
-        infoListView.setAdapter(adapter);
-        imageView.setImageResource(R.drawable.blank);
-
+      case SCAN_IMAGE_REQ:
+        contents = intent.getStringExtra("SCAN_RESULT");
+        break;
+      case SELECT_IMAGE_REQ:
         try
         {
-          int resultID = R.drawable.unknown;
-          boolean expired = false;
+          Uri selectedImage = intent.getData();
 
-          String line;
-          while ((line = reader.readLine()) != null)
+          InputStream imageStream = getContentResolver().openInputStream(
+              selectedImage);
+          Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
+          int width = bitmap.getWidth(), height = bitmap.getHeight();
+          int[] pixels = new int[width * height];
+          bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+
+          try
           {
-            String elements[] = line.split(":");
+            RGBLuminanceSource source = new RGBLuminanceSource(width, height,
+                pixels);
+            BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(
+                source));
+            QRCodeReader qrReader = new QRCodeReader();
+            Result result = qrReader.decode(binaryBitmap);
+            contents = result.getText();
+          }
+          catch (NotFoundException e)
+          {
+            infoTextView.setText("No data found");
+          }
 
-            if (elements.length < 2)
+          processContents(contents);
+
+        }
+        catch (Exception e)
+        {
+          infoTextView.setText(e.getMessage());
+        }
+        break;
+      }
+
+      processContents(contents);
+    }
+  }
+
+  private void processContents(String contents)
+  {
+    String from[] = { "tag", "icon", "value" };
+    int to[] = { R.id.tag, R.id.icon, R.id.value };
+
+    List<Map<String, Object>> items = new ArrayList<Map<String, Object>>();
+
+    SimpleAdapter adapter = new SimpleAdapter(this, items, R.layout.list_view,
+        from, to);
+
+    ListView infoListView = (ListView) findViewById(R.id.list_info);
+    ImageView resultImageView = (ImageView) findViewById(R.id.image_result);
+    TextView infoTextView = (TextView) findViewById(R.id.text_info);
+
+    infoListView.setAdapter(adapter);
+    resultImageView.setImageResource(R.drawable.blank);
+
+    if (contents != null)
+    {
+      BufferedReader reader = new BufferedReader(new StringReader(contents));
+
+      StringBuffer data = new StringBuffer();
+
+      try
+      {
+        int resultID = R.drawable.unknown;
+        boolean expired = false;
+
+        String line;
+        while ((line = reader.readLine()) != null)
+        {
+          String elements[] = line.split(":");
+
+          if (elements.length < 2)
+          {
+            data.append(line);
+            data.append(EOL);
+          }
+          else
+          {
+            String tag = elements[0];
+            String value = elements[1];
+            int iconID = R.drawable.blank_small;
+
+            if ("_SIG".equals(tag))
             {
-              data.append(line);
-              data.append(EOL);
+              Signature instance = Signature.getInstance("ECDSA");
+              instance.initVerify(publicKey);
+              instance.update(data.toString().getBytes());
+              if (instance.verify(Base64.decode(value, Base64.DEFAULT)))
+              {
+                if (expired)
+                  resultID = R.drawable.expired;
+                else
+                  resultID = R.drawable.valid;
+              }
+              else
+                resultID = R.drawable.invalid;
             }
             else
             {
-              String tag = elements[0];
-              String value = elements[1];
-              int iconID = R.drawable.blank_small;
-              
-              if ("_SIG".equals(tag))
+              data.append(line);
+              data.append(EOL);
+
+              if (tag.matches("^[0-9]{4}-[0-9]{2}-[0-9]{2}$"))
               {
-                Signature instance = Signature.getInstance("ECDSA");
-                instance.initVerify(publicKey);
-                instance.update(data.toString().getBytes());
-                if (instance.verify(Base64.decode(value, Base64.DEFAULT)))
+                // We check date tags for expiry, but reorder for layout.
+
+                String tmp = tag;
+                tag = value;
+                value = tmp;
+                Date expires = dateFormatter.parse(value);
+
+                if (expires.before(now))
                 {
-                  if (expired)
-                    resultID = R.drawable.expired;
-                  else
-                    resultID = R.drawable.valid;
+                  iconID = R.drawable.expired_small;
+                  if ("Expiry".equals(tag))
+                    expired = true;
                 }
-                else
-                  resultID = R.drawable.invalid;
               }
-              else
-              {
-                data.append(line);
-                data.append(EOL);
 
-                if (tag.matches("^[0-9]{4}-[0-9]{2}-[0-9]{2}$"))
-                {
-                  // We check date tags for expiry, but reorder for layout.
+              Map<String, Object> map = new HashMap<String, Object>();
 
-                  String tmp = tag;
-                  tag = value;
-                  value = tmp;
-                  Date expires = dateFormatter.parse(value);
+              map.put("tag", tag);
+              map.put("icon", iconID);
+              map.put("value", value);
 
-                  if (expires.before(now))
-                  {
-                    iconID = R.drawable.expired_small;
-                    if ("Expiry".equals(tag))
-                      expired = true;
-                  }
-                }
-                
-                Map<String, Object> map = new HashMap<String, Object>();
-
-                map.put("tag", tag);
-                map.put("icon", iconID);
-                map.put("value", value);
-
-                items.add(map);
-              }
+              items.add(map);
             }
           }
-
-          imageView.setImageResource(resultID);
-
-        } catch (Exception e)
-        {
-          Log.e(T, e.toString());
         }
+
+        resultImageView.setImageResource(resultID);
+
+      }
+      catch (Exception e)
+      {
+        infoTextView.setText(e.getMessage());
       }
     }
   }
